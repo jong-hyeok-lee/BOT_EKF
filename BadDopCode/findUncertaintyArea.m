94a7b3003bf51765a3bd6f7ca52df994e0787039
jhl01 <jhl01@DESKTOP-D5D10G0>
2024-11-14 13:51:57 +0900

    자동으로 threshold를 튜닝해줌

--- BadDopCode/findUncertaintyArea.m
+++ BadDopCode/findUncertaintyArea.m
@@ -1,8 +1,15 @@
+% Find Uncertainty Area Function
 function [uncertainty, eigenVect, eigenVal, condNum] = findUncertaintyArea(sensor1, sensor2, sensor3, th)
     % sensor1, sensor2, sensor3: 구조체, 각각 points (Point Cloud), P (위치), D (방향) 등 포함
     % th: 거리 임계값 (겹치는 영역 기준)
     % uncertainty: 세 UUV의 겹치는 영역 (공통 포인트)
-
+    checkpoint_a=length(sensor1.points)/2;
+    checkpoint_b=checkpoint_a+1;
+    
+    th = mean([norm(sensor1.points(checkpoint_a)-sensor1.points(checkpoint_b))...
+            ,norm(sensor2.points(checkpoint_a)-sensor2.points(checkpoint_b)) ...
+            ,norm(sensor3.points(checkpoint_a)-sensor3.points(checkpoint_b))]);
+    disp(th)
     % Step 1: Sensor 1과 Sensor 2 간의 겹치는 영역 계산
     distances12 = pdist2(sensor1.points, sensor2.points);
     [UUV1_matchIdx_12, ~] = find(distances12 < th);
